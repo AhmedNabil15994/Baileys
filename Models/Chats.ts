cfb8a3a11fe38cf5649f50707de742e4176f6593
AhmedNabil94 <mody998866@gmail.com>
2023-02-06 22:50:59 +0200

    Fix

--- Models/Chats.ts
+++ Models/Chats.ts
@@ -132,7 +132,7 @@ export default class Chats extends Helper {
                         if(Number(dialog.pinned) > 0){
                             pinned.push(dataObj)
                         }else{
-                            if(typeof dataObj.lastMessage !== 'undefined' && dataObj.lastMessage.length){
+                            if(dataObj.lastMessage.length > 0){
                                 notPinned.push(dataObj)
                             }
                         }
